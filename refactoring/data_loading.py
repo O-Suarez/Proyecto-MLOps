62bdaf2a8214ff739fe7a76798337452d1016aee
DiegoMont <diegomonmar@hotmail.com>
2024-10-13 18:34:36 -0600

    Improving code naming

--- refactoring/data_loading.py
+++ refactoring/data_loading.py
@@ -2,6 +2,6 @@ import pandas as pd
 from pandas import DataFrame
 
 
-def load_data(data_path: str) -> DataFrame:
+def load_wikipedia(data_path: str) -> DataFrame:
     df = pd.read_csv(data_path)
     return df
--- refactoring/evaluation.py
+++ refactoring/evaluation.py
@@ -1,10 +1,10 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_squared_error, classification_report, accuracy_score
+from sklearn.metrics import root_mean_squared_error, classification_report, accuracy_score
 
 
 def evaluate_model(model: RandomForestClassifier, X_test, y_test):
     y_pred_rf = model.predict(X_test)
-    mse_rf = mean_squared_error(y_test, y_pred_rf)
+    mse_rf = root_mean_squared_error(y_test, y_pred_rf)
     accuracy_rf = accuracy_score(y_test, y_pred_rf)
     report_rf = classification_report(y_test, y_pred_rf)
     print(f"Error Cuadrático Medio (MSE) - Random Forest: {round(mse_rf, 4)}")
