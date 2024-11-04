2653393d18c6e63ed9bcbad550172c58f5167952
DiegoMont <diegomonmar@hotmail.com>
2024-11-03 18:43:30 -0600

    API integration tests

---
+++ tests/integration/test_api.py
@@ -0,0 +1,26 @@
+import requests
+
+
+BASE_URL = "http://dev.logaritmia.mx:8887"
+
+
+def test_api_is_running():
+    r = requests.get(f"{BASE_URL}/api")
+    assert r.status_code == 200
+
+
+def test_predict():
+    r = requests.post(
+        f"{BASE_URL}/predict",
+        json={
+            "classifier_type": "random_forest",
+            "hyperparameters": {},
+            "NEds": 5,
+            "NActDays": 10,
+            "pagesWomen": 3,
+            "wikiprojWomen": 4,
+            "additional_data": []
+        })
+    assert r.status_code == 200
+    body = r.json()
+    assert body["predictions"][0] == 2
